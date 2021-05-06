f8871e60525446af22ab640e986db72ed25e4b90
shallon kobusinge <kobshallon041@gmail.com>
2021-05-06 18:59:04 +0200

    added env page

--- Server/routers/transaction.router.js
+++ Server/routers/transaction.router.js
@@ -1,7 +1,5 @@
 const transactions = require('../controller/transaction.controller.js')
-const card = require('../controller/card.controller.js')
 module.exports = (app) => {
     app.post('/api/create/transactions',transactions.createTransation);
     app.get('/api/read/transactions', transactions.getTransactions)
-    app.post('/api/create/card', card.createCard)
 }
